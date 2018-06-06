09a5cc04be4eafc6641e4595c3913956db737677
kniven51 <kaleb.niven51@gmail.com>
2018-06-06 14:00:55 -0500

    add all files

---
+++ member.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Member</title>
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<?php
+	session_start();
+	include 'sql.php';
+	if (isset($_SESSION['username'])) {
+		echo "<div class='loginbox'>Member: ".$_SESSION['username']."</div>";	
+		?>
+	<table width ="50%" cellspacing="5" cellpadding="5" border="1">
+	<th>ID</th>
+	<th>Client Name </th>
+	<?php
+		$sql = "Select  * from clients ";
+		$client_q = mysql_query($sql) or die('Error reading client table');
+
+		// Mysql_num_row is counting table row
+		if (mysql_num_rows($client_q) >= 1) {
+			while ($row = mysql_fetch_array($client_q)) { ?>
+<tr>
+<td><?php echo $row["id"]; ?></td>
+<td><?php echo $row["client_name"]; ?></td>
+				 </tr>
+			<?php }
+		}
+		?>
+		</table>
+		<?php
+	}
+	else {
+		header('location: index.php');
+	}
+	?>
+</body>
+</html>
---
+++ test.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2018 at 08:14 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `client_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `client_name`) VALUES
+(1, 'Monkedia'),
+(2, 'Andrew'),
+(3, 'Micheal'),
+(4, 'Sharon'),
+(5, 'John');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'Kaleb', 'Niven'),
+(2, 'client1', 'client123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
